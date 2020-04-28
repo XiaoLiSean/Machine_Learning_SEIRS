5fe43b6674371d03e403bf146a1697466c53ce1a
XiaoLiSean <hsiao.li.sean@gmail.com>
2020-04-28 14:32:31 -0400

    Comparision code added
    Comparison of DMD and Koopman for different data selection period

--- compare.m
+++ compare.m
@@ -5,8 +5,8 @@ load('prediction'); % Data from NN
 X   = X';
 
 % Generate training data
-train_size  = 20; % size of trainning data
-x_idx   = randperm(1000);
+train_size  = 300; % size of trainning data
+x_idx   = 1:train_size;
 T_train = T(x_idx(1:train_size));
 X_train = X(:,x_idx(1:train_size));
 Y_train = X(:,x_idx(1:train_size)+1);
@@ -57,6 +57,7 @@ err_DMD     = X - X_DMD;
 err_KOOP    = X - X_KOOP;
 
 %% Plot
+% DMD
 % True
 figure(1);
 plot(T,X,'LineWidth',2); hold on;
@@ -67,36 +68,21 @@ hl.NumColumns   = 3;
 xlabel('Time [Days]'); ylabel('Population [People]')
 set(gca,'Fontsize',16); grid on;
 
-% NN
-figure(2);
-subplot(2,1,1)
-plot(t_pred, prediction,'LineWidth',2);
-title('NN Prediction: 4000 epoch / batch size 20');
-hl  = legend('S','E','I_1','I_2','H','R','D','Traning Point');
-hl.NumColumns   = 3;
-xlabel('Time [Days]'); ylabel('Population [People]')
-set(gca,'Fontsize',16); grid on;
-subplot(2,1,2)
-plot(t_pred, err_NN,'LineWidth',2);
-xlabel('Time [Days]'); ylabel('Error [People]')
-set(gca,'Fontsize',16); grid on;
-
-% DMD
-figure(3);
-subplot(2,1,1)
+figure(2)
+subplot(3,2,1)
 plot(T,X_DMD,'LineWidth',2);
-title(['DMD Prediction: ', num2str(train_size), ' Data Points / ', num2str(eig_tru), ' Eigen-modes'])
+title(['DMD Prediction: ', num2str(eig_tru), ' Eigen-modes'])
 hl  = legend('S','E','I_1','I_2','H','R','D','Traning Point');
 hl.NumColumns   = 3;
 xlabel('Time [Days]'); ylabel('Population [People]')
 set(gca,'Fontsize',16); grid on;
-subplot(2,1,2)
+subplot(3,2,3)
 plot(T, err_DMD,'LineWidth',2);
 xlabel('Time [Days]'); ylabel('Error [People]')
 set(gca,'Fontsize',16); grid on;
 
 % DMD eig
-figure(4)
+subplot(3,2,5)
 plot(real(diag(D)), imag(diag(D)), 'x',...
      'MarkerSize', 10, 'LineWidth', 2); hold on;
 plot(cos(0:0.1:2*pi+0.5), sin(0:0.1:2*pi+0.5), 'g', 'LineWidth', 1); 
@@ -105,50 +91,23 @@ title('Eigenvalues of DMD'); xlabel('Re(\lambda)'); ylabel('Im(\lambda)');
 set(gca,'Fontsize',16);
 
 % Koopman
-figure(5);
-subplot(2,1,1)
+subplot(3,2,2)
 plot(T,X_KOOP,'LineWidth',2);
-title(['Koopman Prediction: ', num2str(train_size), ' Data Points'])
+title(['Koopman Prediction: '])
 hl  = legend('S','E','I_1','I_2','H','R','D','Traning Point');
 hl.NumColumns   = 3;
 xlabel('Time [Days]'); ylabel('Population [People]')
 set(gca,'Fontsize',16); grid on;
-subplot(2,1,2)
+subplot(3,2,4)
 plot(T, err_KOOP,'LineWidth',2);
 xlabel('Time [Days]'); ylabel('Error [People]')
 set(gca,'Fontsize',16); grid on;
 
 % Koopman eigen
-figure(6)
+subplot(3,2,6)
 plot(real(diag(Lambda)), imag(diag(Lambda)), 'x',...
      'MarkerSize', 10, 'LineWidth', 2); hold on;
 plot(cos(0:0.1:2*pi+0.5), sin(0:0.1:2*pi+0.5), 'g', 'LineWidth', 1); 
 grid on; pbaspect([1 1 1]);
 title('Eigenvalues of Koopman'); xlabel('Re(\lambda)'); ylabel('Im(\lambda)'); 
 set(gca,'Fontsize',16);
-
-% % All
-% figure(7);
-% subplot(2,2,1)
-% plot(T,X,'LineWidth',2); hold on;
-% plot(T_train,X_train,'ro','LineWidth',2);
-% title('True Data: '); 
-% hl  = legend('S','E','I_1','I_2','H','R','D','Traning Point');
-% hl.NumColumns   = 3;
-% xlabel('Time [Days]'); ylabel('Population [People]')
-% set(gca,'Fontsize',16); grid on;
-% subplot(2,2,2)
-% plot(t_pred, prediction,'LineWidth',2);
-% title('NN Prediction: 4000 epoch / batch size 20');
-% xlabel('Time [Days]'); ylabel('Population [People]')
-% set(gca,'Fontsize',16); grid on;
-% subplot(2,2,3)
-% plot(T,X_DMD,'LineWidth',2);
-% title(['DMD Prediction: ', num2str(train_size), ' Data Points / ', num2str(eig_tru), ' Eigen-modes'])
-% xlabel('Time [Days]'); ylabel('Population [People]')
-% set(gca,'Fontsize',16); grid on;
-% subplot(2,2,4)
-% plot(T,X_KOOP,'LineWidth',2);
-% title(['Koopman Prediction: ', num2str(train_size), ' Data Points'])
-% xlabel('Time [Days]'); ylabel('Population [People]')
-% set(gca,'Fontsize',16); grid on;
